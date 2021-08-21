4a5ab1b7a24d4398226d8ed74250480fa4967cd3
Marc <marcjacques@me.com>
2021-08-21 09:34:33 -0400

    add SafeAreaView to App.js

--- App.js
+++ App.js
@@ -1,23 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
 console.log("App executed");
 
   return (
-    <View style={styles.container}>
+    // this is how you setup a view. 
+    <SafeAreaView style={styles.container}> 
       <Text>Hello React Native</Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1, //the view is flexible and grows to fit up to the edges of the screen 
+    backgroundColor: 'darkturquoise',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
