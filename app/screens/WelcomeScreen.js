b6c79ba6c81910a235da413653e3a20ad6936642
Marc <marcjacques@me.com>
2021-08-21 16:29:38 -0400

    update logo to center
    - add tagline text below logo
    - place logo and text in a container to better organize
    - add center alignment and absolute positioning

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Image, ImageBackground, StyleSheet, View } from "react-native";
+import { Image, ImageBackground, StyleSheet, View, Text } from "react-native";
 
 function WelcomeScreen(props) {
   return (
@@ -7,7 +7,10 @@ function WelcomeScreen(props) {
       style={styles.background}
       source={require("../assets/background.jpg")}
     >
-      <Image source={require("../assets/logo-red.png")} />
+      <View style={styles.logoContainer}>
+        <Image style={styles.logo} source={require("../assets/logo-red.png")} />
+        <Text>"Sell What You Don't Need!"</Text>
+      </View>
       <View style={styles.loginButton}></View>
       <View style={styles.registerButton}></View>
     </ImageBackground>
@@ -18,11 +21,21 @@ const styles = StyleSheet.create({
   background: {
     flex: 1,
     justifyContent: "flex-end",
+    alignItems: "center",
   },
   loginButton: {
     width: "100%",
     height: 70,
     backgroundColor: "#fc5c65",
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+  logoContainer: {
+    position: "absolute",
+    top: 70,
+    alignItems: "center",
   },
   registerButton: {
     width: "100%",
